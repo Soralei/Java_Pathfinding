2c01943cd84ae6dcf8b975b4a75f470beec285c8
Soralei <skollerblades@gmail.com>
2023-05-18 13:50:25 +0200

    Introduced basic GUI and made sure everything functions at a basic level

---
+++ src/main/java/com/example/pathfinding/AStarGUI.java
@@ -0,0 +1,41 @@
+package main.java.com.example.pathfinding;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class AStarGUI {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("A* GUI");
+        jFrame.setSize((100 + 5) * 10, (100 + 5) * 10);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        AStarMain aStarMain = new AStarMain();
+        ArrayList<AStarNode> path = aStarMain.findPath(new Vector2D(0, 0), new Vector2D(50, 99));
+
+        int OFFSET = 1;
+        int SQUARE_SIZE = 10;
+        int PATH_SIZE = 5;
+
+        jFrame.setContentPane(new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.BLACK);
+                for(int i = 0; i < 100 * 100; i++) {
+                    int x = i % 100;
+                    int y = i / 100;
+                    g.drawRect((x + OFFSET) * SQUARE_SIZE - (SQUARE_SIZE/2), (y + OFFSET) * SQUARE_SIZE - (SQUARE_SIZE/2), SQUARE_SIZE, SQUARE_SIZE);
+                }
+
+                g.setColor(Color.GREEN);
+                if(path != null) {
+                    path.forEach(node -> g.fillRect((node.getVector2D().x() + OFFSET) * SQUARE_SIZE - (PATH_SIZE/2), (node.getVector2D().y() + OFFSET) * SQUARE_SIZE - (PATH_SIZE/2), PATH_SIZE, PATH_SIZE));
+                }
+            }
+        });
+
+        jFrame.setVisible(true);
+    }
+}
--- src/main/java/com/example/pathfinding/AStarMain.java
+++ src/main/java/com/example/pathfinding/AStarMain.java
@@ -8,26 +8,33 @@ public class AStarMain {
     int MOVEMENT_VERTICAL_COST = 10;
     int MOVEMENT_DIAGONAL_COST = 14;
 
-    public void findPath(Vector2D start, Vector2D target) {
+    public ArrayList<AStarNode> findPath(Vector2D start, Vector2D target) {
+        // Min heap for O(log n) time complexity.
         final PriorityQueue<AStarNode> openNodes = new PriorityQueue<>(new AStarNodeComparator());
+        final ArrayList<AStarNode> closedNodes = new ArrayList<>();
 
+        // Rather than tracking all nodes on the grid, create nodes as we scan them and store them by index here.
         AStarNode[] nodes = new AStarNode[WIDTH * HEIGHT];
         int index = start.x() + start.y() * WIDTH;
 
+        // Set up the initial node to start the algorithm from.
         AStarNode initial = new AStarNode(start);
         initial.setgScore(0);
         initial.setfScore(heuristicDistance(start, target));
-
         nodes[index] = initial;
         openNodes.add(nodes[index]);
 
+        // Start popping nodes and perform A*.
         AStarNode current;
         while((current = openNodes.poll()) != null) {
             // If the current node's position equals the target, we found our goal.
+            // Begin reconstructing the path.
             if(current.getVector2D().equals(target)) {
-                System.out.println("Found a path to the goal.");
-                return;
+                return constructPath(current);
             }
+
+            // Register the node as closed so that it can be ignored in future searches.
+            closedNodes.add(current);
 
             // Check adjacent positions of current.
             int currentIndex = current.getVector2D().x() + current.getVector2D().y() * WIDTH;
@@ -38,45 +45,73 @@ public class AStarMain {
                     int neighbourY = current.getVector2D().y() + y;
 
                     // Make sure the x and y is within the grid.
-                    if(neighbourX < 0 || neighbourX > WIDTH || neighbourY < 0 || neighbourY > HEIGHT) continue;
+                    if(neighbourX < 0 || neighbourX >= WIDTH || neighbourY < 0 || neighbourY >= HEIGHT) continue;
 
                     // Ignore checking current.
-                    if(neighbourX == 0 && neighbourY == 0) continue;
+                    if(x == 0 && y == 0) continue;
 
                     // Get the neighbour node. Create one if it doesn't yet exist.
                     int neighbourIndex = neighbourX + neighbourY * WIDTH;
+                    if(neighbourIndex >= nodes.length || neighbourIndex < 0) continue;
                     if(nodes[neighbourIndex] == null) {
                         nodes[neighbourIndex] = new AStarNode(new Vector2D(neighbourX, neighbourY));
                     }
                     AStarNode neighbour = nodes[neighbourIndex];
 
                     // Calculate scores and check whether the neighbour should be added to the open list for future checking.
-                    int tentative_gScore = current.getgScore() + movementCost(current.getVector2D(), neighbour.getVector2D());
-                    if(tentative_gScore < neighbour.getgScore()) {
-                        neighbour.setParent(current);
-                        neighbour.setgScore(tentative_gScore);
-                        neighbour.setfScore(tentative_gScore + heuristicDistance(neighbour.getVector2D(), target));
+                    int cost = current.getgScore() + movementCost(current.getVector2D(), neighbour.getVector2D());
 
-                        if(!openNodes.contains(neighbour)) {
-                            openNodes.add(neighbour);
-                        }
+                    // Track whether the neighbour is open or closed.
+                    boolean neighbourOpen = openNodes.contains(neighbour);
+                    boolean neighbourClosed = closedNodes.contains(neighbour);
+
+                    // Check if the neighbour at this moment has better gscore than previously registered.
+                    if(neighbourOpen && cost < neighbour.getgScore()) {
+                        openNodes.remove(neighbour);
+                        neighbourOpen = false;
+                    }
+
+                    // Check if the neighbour was closed, but currently has better gscore - if so, don't consider it closed.
+                    // This shouldn't happen with a consistent admissible heuristic.
+                    if(neighbourClosed && cost < neighbour.getgScore()) {
+                        closedNodes.remove(neighbour);
+                        neighbourClosed = false;
+                    }
+
+                    // Set scores, parent, and register the neighbour as an open node.
+                    if(!neighbourOpen && !neighbourClosed) {
+                        neighbour.setgScore(cost);
+                        neighbour.setfScore(neighbour.getgScore() + heuristicDistance(neighbour.getVector2D(), target));
+                        neighbour.setParent(current);
+                        openNodes.add(neighbour);
                     }
                 }
             }
         }
-        // No path was found...
-        System.out.println("Failed to find a path...");
+        // No path was found, return null.
+        return null;
     }
 
     private int heuristicDistance(Vector2D from, Vector2D to) {
         // Square grid standard manhattan diagonal distance calculations
         int dx = Math.abs(from.x() - to.x());
         int dy = Math.abs(from.y() - to.y());
-        return MOVEMENT_VERTICAL_COST * (dx + dy) + (MOVEMENT_VERTICAL_COST - 2 * MOVEMENT_DIAGONAL_COST) * Math.min(dx ,dy);
+        return MOVEMENT_VERTICAL_COST * (dx + dy) + (MOVEMENT_DIAGONAL_COST - 2 * MOVEMENT_VERTICAL_COST) * Math.min(dx, dy);
     }
 
     private int movementCost(Vector2D from, Vector2D to) {
         boolean isDiagonal = from.y() != to.y() && from.x() != to.x();
         return isDiagonal ? MOVEMENT_DIAGONAL_COST : MOVEMENT_VERTICAL_COST;
+    }
+
+    private ArrayList<AStarNode> constructPath(AStarNode from) {
+        ArrayList<AStarNode> path = new ArrayList<>();
+        AStarNode current = from;
+        do {
+            path.add(current);
+            current = current.getParent();
+        } while(current != null);
+
+        return path;
     }
 }
